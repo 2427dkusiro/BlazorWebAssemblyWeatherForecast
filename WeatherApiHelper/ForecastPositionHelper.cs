e7483bf08fafa8f5ec1af207e54e0c44af954a58
康太 神野 <kantake1969@gmail.com>
2021-09-24 16:59:05 +0900

    プロジェクト ファイルを追加します。

---
+++ WeatherApiHelper/ForecastPositionHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeatherApiHelper
+{
+    public static class ForecastPositionHelper
+    {
+        public static ForecastPositionIndex ParseForecastPositionsXml(Stream stream)
+        {
+            string str;
+            using (StreamReader streamReader = new StreamReader(stream))
+            {
+                str = streamReader.ReadToEnd();
+            }
+
+            var xDocument = XDocument.Parse(str);
+            var groups = xDocument.Root.Elements()
+                 .Select(pref =>
+                 {
+                     var cities = pref.Elements().Where(elem => elem.Name == "city")
+                     .Select(city =>
+                     {
+                         string cityName = city.Attribute("title").Value;
+                         int id = int.Parse(city.Attribute("id").Value);
+                         return new ForecastPosition(cityName, id);
+                     }).ToArray();
+                     string prefName = pref.Attribute("title").Value;
+                     var group = new ForecastPositionGroup(prefName, cities);
+                     return group;
+                 });
+
+            var dict = groups.SelectMany(x => x.ForecastPositions).ToDictionary(x => x.CityName, x => x.Id);
+            return new ForecastPositionIndex(dict, groups.ToArray());
+        }
+    }
+
+    public record ForecastPositionIndex(Dictionary<string, int> PositionDictionary, IList<ForecastPositionGroup> PositionGroups);
+
+    public record ForecastPosition(string CityName, int Id);
+
+    public record ForecastPositionGroup(string GroupName, IList<ForecastPosition> ForecastPositions);
+}
---
+++ WeatherApiHelper/ForecastProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WeatherApiHelper
+{
+    public static class ForecastProvider
+    {
+        public static async Task<WeatherForecast> FetchForecast(HttpClient httpClient, int positionId)
+        {
+            const string url = @"https://weather.tsukumijima.net/api/forecast";
+            string query = $"?city={positionId.ToString("D6")}";
+
+            return await httpClient.GetFromJsonAsync<WeatherForecast>(url + query);
+        }
+    }
+
+    public class WeatherForecast
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("forecasts")]
+        public WeatherForecastBody[] Forecasts { get; set; }
+    }
+
+    public class WeatherForecastBody
+    {
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("telop")]
+        public string Telop { get; set; }
+
+        [JsonPropertyName("temperature")]
+        public TemperatureOfDay Temperature { get; set; }
+    }
+
+    public class TemperatureOfDay
+    {
+        [JsonPropertyName("min")]
+        public Temperature Min { get; set; }
+
+        [JsonPropertyName("max")]
+        public Temperature Max { get; set; }
+    }
+
+    public class Temperature
+    {
+        [JsonPropertyName("celsius")]
+        public decimal? Celsius { get; set; }
+
+        [JsonPropertyName("fahrenheit")]
+        public decimal? Fahrenheit { get; set; }
+    }
+}
